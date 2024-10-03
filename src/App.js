81d72d3589738ecdd9013b4d349739a4332648b9
Agustín Peruzzi <agustin.peruzzi.latinad@gmail.com>
2024-10-02 21:24:54 -0300

    Add tests and pipelines

--- src/App.js
+++ src/App.js
@@ -1,43 +1,58 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [selectedOption, setSelectedOption] = useState('');
-  const [numberInput, setNumberInput] = useState('');
-  const [response, setResponse] = useState('');
+  const [selectedOption, setSelectedOption] = useState("inch_to_cm");
+  const [numberInput, setNumberInput] = useState(1);
+  const [response, setResponse] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const res = await fetch(`http://localhost:3000/${selectedOption}/${numberInput}`, {
-        method: 'GET'
-      });
+      const res = await fetch(
+        `http://localhost:3000/${selectedOption}/${numberInput}`,
+        {
+          method: "GET",
+        }
+      );
 
       const data = await res.json();
-      setResponse(data.message || 'Success!');
-
-
-      alert(data.result);
+      setResponse(data.result || "Success!");
 
     } catch (error) {
-      setResponse('An error occurred.');
+      setResponse("An error occurred.");
     }
   };
 
   return (
     <div className="App">
-      <h1>Conversor de unidades fullstack</h1>
+      <h1>Conversor de unidades full-stack</h1>
       <form onSubmit={handleSubmit}>
         <label>
           Select an option:
-          <select value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)}>
-            <option value="inch_to_cm">Pulgadas a centímetros</option>
-            <option value="cm_to_inch">Centímetros a pulgadas</option>
-            <option value="fahrenheit_to_celsius">Fahrenheit a centígrados</option>
-            <option value="celsius_to_fahrenheit">Centígrados a fahrenheit</option>
-            <option value="pounds_to_kgs">Libras a kilos</option>
-            <option value="kgs_to_pounds">Kilos a libras</option>
+          <select
+            value={selectedOption}
+            onChange={(e) => setSelectedOption(e.target.value)}
+          >
+            <option value="inch_to_cm">
+              Pulgadas a centímetros
+            </option>
+            <option value="cm_to_inch">
+              Centímetros a pulgadas
+            </option>
+            <option value="fahrenheit_to_celsius">
+              Fahrenheit a centígrados
+            </option>
+            <option value="celsius_to_fahrenheit">
+              Centígrados a fahrenheit
+            </option>
+            <option value="pounds_to_kgs">
+              Libras a kilos
+            </option>
+            <option value="kgs_to_pounds">
+              Kilos a libras
+            </option>
           </select>
         </label>
         <br />
@@ -52,6 +67,9 @@ function App() {
         <br />
         <button type="submit">Convertir</button>
       </form>
+
+      <h2>Resultado: {response}</h2>
+
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,15 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // get button and click it
+  const submitButton = screen.getByText(/Convertir/i);
+  submitButton.click();
+  // wait 10 seconds
+  setTimeout(() => {
+    // get response and check it
+    const response = screen.getByText(/Resultado: 2.54/i);
+    expect(response).toBeInTheDocument();
+  }, 10000);
+
+
 });
